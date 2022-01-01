0859a60b587665cab748db0cb5c448942e66c6d4
GloryDay <alwaysgloryday@gmail.com>
2022-01-02 00:15:16 +0900

    Edit application files and folders

--- application/src/application.js
+++ application/src/application.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
+// Use the following two modules to import HTML files built in a deployment environment
 var path = require("path");
 var url = require("url");
 var electron_1 = require("electron");
@@ -12,12 +13,17 @@ function createMainWindow() {
         height: 720,
         resizable: false,
         titleBarStyle: "hidden",
+        // It means combining the Node environment with the Electron to use the above path and url module.
         webPreferences: {
             nodeIntegration: true
         }
     });
+    // Actually deployed applications serve the built index.html file.
+    // Objects that appear as url.pathToFileURL() need to be converted to string type.
     var mainWindowUrl = url.pathToFileURL(path.join(__dirname, '../build/index.html')).toString();
+    // Check the development environment and serve with the correct url/file.
     mainWindow.loadURL(isDev ? baseUrl : mainWindowUrl);
+    // For the development environment, open and use Chrome's developer tool.
     if (isDev) {
         mainWindow.webContents.openDevTools();
     }
@@ -25,9 +31,11 @@ function createMainWindow() {
         mainWindow = null;
     });
 }
+// If the application is ready, run it as a desktop application.
 electron_1.app.on("ready", function () {
     createMainWindow();
 });
+// If all windows are closed, exit the application.
 electron_1.app.on("window-all-closed", function () {
     electron_1.app.quit();
 });
